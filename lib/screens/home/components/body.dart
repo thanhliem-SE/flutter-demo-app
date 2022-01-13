85d03602414b7edbe8fd3b1b74fb5a80b21bf6a2
thanhliem-SE <thanhliem2n@gmail.com>
2022-01-13 09:51:55 +0700

    add UI category_list and category_item

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_ordering_app/components/search_box.dart';
+import 'package:food_ordering_app/constants.dart';
+import 'package:food_ordering_app/screens/home/components/category_list.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -9,7 +12,73 @@ class Body extends StatelessWidget {
     return Column(
       children: [
         SearchBox(onChanged: (value) {}),
+        const CategoryList(),
+        Row(
+          children: const <Widget>[
+            // ItemCart(),
+          ],
+        ),
       ],
+    );
+  }
+}
+
+class ItemCart extends StatelessWidget {
+  final String shopName, title, svgSrc;
+  final VoidCallback function;
+  const ItemCart({
+    Key? key,
+    required this.shopName,
+    required this.title,
+    required this.svgSrc,
+    required this.function,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 15, bottom: 20, top: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, 4),
+            blurRadius: 20,
+            color: const Color(0xFFB0CCE1).withOpacity(0.32),
+          )
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: function,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 15),
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.13),
+                  shape: BoxShape.circle,
+                ),
+                child: SvgPicture.asset(
+                  svgSrc,
+                  width: size.width * 0.18,
+                ),
+              ),
+              Text(title),
+              const SizedBox(height: 10),
+              Text(
+                shopName,
+                style: const TextStyle(fontSize: 12),
+              ),
+            ]),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/home/components/category_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering_app/screens/home/components/category_item.dart';
+
+class CategoryList extends StatelessWidget {
+  const CategoryList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CategoryItem(
+            title: "Combo Meal",
+            isActive: true,
+            press: () {},
+          ),
+          CategoryItem(
+            title: "Chicken",
+            isActive: false,
+            press: () {},
+          ),
+          CategoryItem(
+            title: "Beverages",
+            isActive: false,
+            press: () {},
+          ),
+          CategoryItem(
+            title: "Snacks & Sides",
+            isActive: false,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
