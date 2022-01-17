3e02b9f31590a37200d58ee1cfc2f1e1e89ab4ad
thanhliem-SE <thanhliem2n@gmail.com>
2022-01-17 16:15:48 +0700

    add fetch data from api

--- lib/views/manager/category_manage_screen.dart
+++ lib/views/manager/category_manage_screen.dart
@@ -35,8 +35,79 @@ class _CategoryManageState extends State<CategoryManage> {
           },
           color: Colors.black,
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add_rounded),
+            color: Colors.green,
+            iconSize: 50,
+          ),
+        ],
       ),
       bottomNavigationBar: const BottomNavBar(),
+      body: Column(
+        children: [
+          FutureBuilder<List<Category>>(
+            future: futureCategoryList,
+            builder: (context, future) {
+              if (!future.hasData) {
+                return Container();
+              } // return empty container
+              else {
+                List<Category>? categoryList = future.data;
+                return Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: categoryList?.length,
+                    itemBuilder: (context, index) {
+                      String? name = categoryList?[index].name;
+                      String? img = categoryList?[index].img;
+                      Size size = MediaQuery.of(context).size;
+                      return Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.network(
+                              img!,
+                              fit: BoxFit.fill,
+                              height: size.height * 0.15,
+                              width: size.width * 0.2,
+                            ),
+                            SizedBox(
+                              width: size.width * 0.3,
+                              child: Text(
+                                name!,
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.edit),
+                              color: Colors.blue,
+                              iconSize: 40,
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.delete),
+                              color: Colors.red,
+                              iconSize: 40,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
